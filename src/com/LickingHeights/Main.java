1d589d096e47f537438c49162cc72145269f1576
lizzie <elizabeth.minturn.2021@student.lhschools.org>
2019-03-01 07:35:04 -0500

    created max number method and min number method

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -5,13 +5,10 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        //5)method that finds the max double between numbers 6)switch words
 
         //declare
         Scanner keyboard;
         String name;
-        String wordA;
-        String wordB;
 
         //initialize
         keyboard = new Scanner(System.in);
@@ -26,35 +23,45 @@ public class Main {
         int c = 15;
         int d = 3;
         System.out.println("15/3 is " + divide(c,d));
-        wordA = "hello";
-        wordB = "world";
-        Switch(wordA,wordB);
+        max(a,b);
+        min(a,b);
+        Switch("hello", "world");
 
         }
 
         public static void printGreeting(String word){
+
         System.out.println("hello " + word);
         }
 
         public static String helloName(String name){
-        System.out.println("hello " + name);
-        return name;
+
+        return "hello "+name;
         }
 
         public static int multiply(int firstNumber, int secondNumber){
+
         return firstNumber*secondNumber;
         }
 
-    public static int divide(int firstNumber, int secondNumber){
+        public static int divide(int firstNumber, int secondNumber){
+
         return firstNumber/secondNumber;
         }
 
-        public static int max(int firstNumber, int secondNumber) {
+        public static void max(int a, int b){
 
+        System.out.println(Math.max(a,b));
+        }
+
+        public static void min(int a, int b){
+
+        System.out.println(Math.min(a,b));
         }
 
         public static void Switch(String wordA, String wordB){
-        System.out.println(wordB + " " + wordA);
+
+        System.out.println(wordB+" "+wordA);
         }
 
     }
